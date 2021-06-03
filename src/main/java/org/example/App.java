e037776d59cde69cd3db2bb3d3199ca76a6ef98d
Anisha Desktop <anisharanjan55@gmail.com>
2021-06-03 00:25:41 -0400

    Initial solution to problem 1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,25 @@
 package org.example;
+import java.util.Scanner;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = in.nextLine();
+
+        String outputString = generateOutputString(name);
+        //String outputString = "Hello, " + name + ", nice to meet you!";
+        if(outputString.equals("Hello, Brian, nice to meet you!")) {
+            System.out.println("Test passed!");
+        } else {
+            System.out.println("Test failed");
+        }
+
+
+    }
+    private static String generateOutputString(String name) {
+        return "Hello, " + name + ", nice to meet you!";
     }
 }
+
